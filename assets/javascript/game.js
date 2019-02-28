a3490c1b60c2cb8f7b2614f8ac9f2363058bd459
Jessica Jones <47366649+vguercio10@users.noreply.github.com>
2019-02-28 17:36:54 -0500

    game complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,38 +3,58 @@
 var wins = 0
 var loses = 0
 var guesssofar = []
-var guessesremaining = 10
+var guessremaining = 10
 var computerguess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    var computerchoice = computerguess[Math.floor(Math.random() * computerguess.length)];
+var computerchoice = computerguess[Math.floor(Math.random() * computerguess.length)]
+
+// Variables that show in HTML
+// var winsText = document.getElementById("wins-text");
+// var losesText = document.getElementById("loses-text");
+// var userguessText = document.getElementById("userguess-text");
+// var guessremainingText = document.getElementById("guessremaining-text");
+// var userchoiceText = document.getElementById("userchoice-text");
+
 
 // Begin Game ---------------------------------
  document.onkeyup = function(event) {
-    guesssofar.push(event.key)    
+    guesssofar.push(event.key);    
     var userguess = event.key;
     var userlower = userguess.toLowerCase();
 // When user wins------------------------------
 if (userlower === computerchoice) {
     wins++; 
+    guesssofar = [];
+    guessremaining = 10;
     console.log(wins);
 }
 
 // When user loses-----------------------------
-else (userlower !== computerchoice)
-    guessesremaining--;
+else
+    guessremaining--;
+    
     console.log(loses);
 
 
- if (guessesremaining === 0) {
+ if (guessremaining === 0) {
      loses++;
-     guessesremaining = 10;
+     guessremaining = 10;
      guesssofar = [];
  }
 
+ var HTML=
+ "<p>wins: " + wins + "</p>" +
+ "<p>losses: " + loses + "</p>" +
+ "<p>guesses remaining: " + guessremaining + "</p>" +
+ "<p>guesses so far: " + guesssofar + "</p>" ;
+
+ document.querySelector("#game").innerHTML = HTML;
+
+
 //  Display the user and computer, guesses, wins, loses. 
-winstext.textContent = "Wins: " + wins;
-losestext.textContent= "Loses: " + loses;
-userguessText.textContent= " Your guesses so far: " + userlower;
-guessesremainingText.textContent= "Number of guesses remaining: " + guessesremaining;
-userchoiceText.textContent= "You chose: " + guesssofar;
+// winsText.textContent = "Wins: " + wins;
+// losesText.textContent= "Loses: " + loses;
+// userguessText.textContent= " Your guesses so far: " + userlower;
+// guessremainingText.textContent= "Number of guesses remaining: " + guessremaining;
+// userchoiceText.textContent= "You chose: " + guesssofar;
 
  }
